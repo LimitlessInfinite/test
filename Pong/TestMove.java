15f3eecaef746a81592aa86bb1c4f09ef67223b2
Bogdan <1218bogdan@gmail.com>
2023-02-26 14:29:45 +0200

    Add rollback

--- Pong/TestMove.java
+++ Pong/TestMove.java
@@ -3,5 +3,6 @@ package Practice.Base.Pong;
 public class TestMove {
     public static void main(String[] args) {
         System.out.println("I don't know what i must do");
+        System.out.println("rollback");
     }
 }
